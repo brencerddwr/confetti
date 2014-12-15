b16abff19ff6e581a599759f960afe6403cf1797
Richard Bailey <rgbailey@gmail.com>
2014-12-15 12:31:02 -0800

    Delete hsv2rgb.cpp.d

--- Debug/FastLED/hsv2rgb.cpp.d
+++
@@ -1,8 +0,0 @@
- \
- C:\Users\Richard\AppData\Local\VMicro\Arduino\Builds\confetti\flora8\FastLED\hsv2rgb.cpp.o:  \
- D:\Users\Richard\Documents\Arduino\libraries\FastLED\hsv2rgb.cpp \
-  D:\Users\Richard\Documents\Arduino\libraries\FastLED\/lib8tion.h \
-  D:\Users\Richard\Documents\Arduino\libraries\FastLED\/hsv2rgb.h \
-  D:\Users\Richard\Documents\Arduino\libraries\FastLED\/pixeltypes.h \
-  D:\Users\Richard\Documents\Arduino\libraries\FastLED\/lib8tion.h \
-  D:\Users\Richard\Documents\Arduino\libraries\FastLED\/color.h
