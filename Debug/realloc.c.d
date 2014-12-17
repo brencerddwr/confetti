a0401e9dc06409b8c162a01a3ddd0d3ff988077c
brencerddwr <rgbailey@gmail.com>
2014-12-16 21:58:28 -0800

    cleanup of atmel studio files on github

--- Debug/realloc.c.d
+++
@@ -1,5 +0,0 @@
- \
- C:\Users\Richard\AppData\Local\VMicro\Arduino\Builds\confetti\flora8\realloc.c.o:  \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\avr-libc\realloc.c \
-  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\avr-libc\/sectionname.h \
-  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\avr-libc\/stdlib_private.h
