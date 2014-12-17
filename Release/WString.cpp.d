b6009305d7d63b8a22ed78daf59db176b16758ca
brencerddwr <rgbailey@gmail.com>
2014-12-16 22:01:39 -0800

    cleanup of github

--- Release/WString.cpp.d
+++
@@ -1,4 +0,0 @@
- \
- C:\Users\Richard\AppData\Local\VMicro\Arduino\Builds\confetti\flora8\WString.cpp.o:  \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\WString.cpp \
-  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\/WString.h
--- Release/new.cpp.d
+++
@@ -1,4 +0,0 @@
- \
- C:\Users\Richard\AppData\Local\VMicro\Arduino\Builds\confetti\flora8\new.cpp.o:  \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\new.cpp \
-  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/new.h
